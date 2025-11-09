5e41f41d4b56cc6fd028996db384b0d9ab89c4cd
Atharva Project <atharvaproject2005@gmail.com>
2025-11-09 13:37:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4723488412-ac075",
+  "appId": "1:17266240659:web:92be3e8e4b46fe4dcb3166",
+  "apiKey": "AIzaSyCrqLmtcLISBmnyHj40bhjszkryxifpx5U",
+  "authDomain": "studio-4723488412-ac075.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "17266240659"
+};
